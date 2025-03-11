9f8667fd8c8c560a7db6d30d82247d043815832f
shtirlitsDva <xopkop@gmail.com>
2025-03-11 13:50:26 +0100

    working on wpf window

--- ABF_SheetSetManager/SheetManager/Views/SheetManagerWindow.xaml.cs
+++ ABF_SheetSetManager/SheetManager/Views/SheetManagerWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using HandyControl.Controls;
 using HandyControl.Themes;
 
+using SheetSetManager.SheetManager.Models;
 using SheetSetManager.SheetManager.ViewModels;
 
 using System;
@@ -9,6 +10,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 using Window = HandyControl.Controls.Window;
 
@@ -29,6 +33,34 @@ namespace SheetSetManager.SheetManager.Views
             DataContext = vm;
 
             ThemeManager.Current.ApplicationTheme = ApplicationTheme.Dark;
+        }
+
+        private void CheckBox_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Prevent row expansion when clicking the checkbox
+            e.Handled = true;
+
+            // Toggle the checkbox manually
+            if (sender is CheckBox checkBox)
+            {
+                checkBox.IsChecked = !checkBox.IsChecked;
+            }
+        }
+
+        private void DataGrid_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space) // Check if Spacebar is pressed
+            {
+                if (sender is DataGrid dataGrid && dataGrid.SelectedItem is SheetModel selectedSheet)
+                {
+                    selectedSheet.IsSelected = !selectedSheet.IsSelected; // Toggle checkbox state
+                }
+            }
+        }
+
+        private void DataGrid_Sorting(object sender, DataGridSortingEventArgs e)
+        {
+            
         }
     }
 }
